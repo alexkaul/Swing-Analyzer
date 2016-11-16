04e6611317a288ab93b37025ef4dfa3694cbebc3
Alexandra Kaulfuss <alexandra@itstadtlenzsair.fritz.box>
2016-11-16 21:08:49 +0100

    Initial Commit

---
+++ Swing Analyzer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swing Analyzer
+//
+//  Created by Alexandra Kaulfuss on 16.11.16.
+//  Copyright © 2016 Alexandra Kaulfuss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
